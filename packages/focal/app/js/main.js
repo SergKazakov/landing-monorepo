7ac01cef6d278bb17e93b7be00b8abea3b4586a4
SergKazakov <sergeykazakoff@yandex.ru>
2014-06-06 12:59:28 +0400

    Use pixel rerfect Start making focal responsive

--- packages/focal/app/js/main.js
+++ packages/focal/app/js/main.js
@@ -1,3 +1,21 @@
-$(function (){
-	'use strict';
+$(function() {
+    'use strict';
+
+    function getTabs(tabItem, tabContent) {
+        $('.' + tabContent + ':first-child').fadeIn();
+        $('.' + tabItem).on('click', function() {
+            $(this).addClass(tabItem + '--active')
+                .siblings()
+                .removeClass(tabItem + '--active');
+            $('.' + tabContent).eq($(this).index())
+                .fadeIn()
+                .siblings()
+                .hide();
+        });
+    }
+
+    getTabs('tab-controls__item', 'tab-item');
+
+    responsiveNav(".nav-collapse");
+    $('.nav-toggle').html('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
 });
