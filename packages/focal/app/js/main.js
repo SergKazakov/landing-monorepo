80e3bf320e9e22e798f592af5feb36b441bb3bbd
SergKazakov <sergeykazakoff@yandex.ru>
2014-06-07 12:04:17 +0400

    Add selectivizr for ie8 and fix ie8 problems

--- packages/focal/app/js/main.js
+++ packages/focal/app/js/main.js
@@ -1,6 +1,7 @@
 $(function() {
     'use strict';
 
+    //tabs
     function getTabs(tabItem, tabContent) {
         $('.' + tabContent + ':first-child').fadeIn();
         $('.' + tabItem).on('click', function() {
@@ -16,11 +17,25 @@ $(function() {
 
     getTabs('tab-controls__item', 'tab-item');
 
+
+    //nav
     responsiveNav(".nav-collapse");
     $('.nav-toggle').html('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
 
+
+    //slider
     $('.flexslider').flexslider({
         'animation': 'fade',
         'directionNav': false,
+    });
+
+
+    //scroll
+    $('a[href^=#]').on('click', function() {
+        var href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, 1000);
+        return false;
     });
 });
