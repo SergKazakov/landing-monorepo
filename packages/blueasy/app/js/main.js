ea11d9ac411dccb5053670f0fa1e513f3b0b8768
SergKazakov <sergeykazakoff@yandex.ru>
2014-06-09 23:01:15 +0400

    Add media queries

--- packages/blueasy/app/js/main.js
+++ packages/blueasy/app/js/main.js
@@ -1,6 +1,31 @@
 $(function() {
     'use strict';
 
+    var windowWidth = $(window).width(),
+        bigScreen = windowWidth > 993,
+        middleScreen = windowWidth < 993 && windowWidth > 480;
+
+    function marginRecalc(mas) {
+        if (bigScreen) {
+            mas.each(function(i, item) {
+                if (i % 4 === 0) {
+                    $(item).css('margin-left', 0);
+                } else {
+                    $(item).css('margin-left', '2.5641%');
+                }
+            });
+        } else if (middleScreen) {
+            mas.each(function(i, item) {
+                if (i % 2 === 0) {
+                    $(item).css('margin-left', 0);
+                } else {
+                    $(item).css('margin-left', '4%');
+                }
+            });
+        }
+        return mas;
+    };
+
     //    filter
     var filterItem = 'filter__item-link';
     $('.' + filterItem).on('click', function(e) {
@@ -14,17 +39,22 @@ $(function() {
             if (dataClass != 'all') {
                 galleryItems.hide();
                 var filterItems = galleryItems.filter('.gallery__item--' + dataClass);
-                filterItems.fadeIn();
-            } else galleryItems.fadeIn();
+                marginRecalc(filterItems).fadeIn();
+            } else {
+                marginRecalc(galleryItems).fadeIn();
+            }
         }
     });
 
     //    nav
-    $('a[href^=#]').on('click', function() {
+    $('.nav__item').on('click', function() {
         var href = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(href).offset().top
         }, 1000);
         return false;
     });
+
+    responsiveNav(".nav-collapse");
+    $('.nav-toggle').html('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
 });
