69a0869c31572d94fea544be4279f24b52c888d8
SergKazakov <sergeykazakoff@yandex.ru>
2014-06-12 16:59:45 +0400

    Add fullscreen img

--- packages/blueasy/app/js/main.js
+++ packages/blueasy/app/js/main.js
@@ -57,4 +57,57 @@ $(function() {
 
     responsiveNav(".nav-collapse");
     $('.nav-toggle').html('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
+
+    /* fix vertical when not overflow
+    call fullscreenFix() if .fullscreen content changes */
+    function fullscreenFix(){
+        var h = $('body').height();
+        // set .fullscreen height
+        $(".content-b").each(function(i){
+            if($(this).innerHeight() <= h){
+                $(this).closest(".fullscreen").addClass("not-overflow");
+            }
+        });
+    }
+    $(window).resize(fullscreenFix);
+    fullscreenFix();
+
+    /* resize background images */
+    function backgroundResize(){
+        var windowH = $(window).height();
+        $(".background").each(function(i){
+            var path = $(this),
+            // variables
+                contW = path.width(),
+                contH = path.height(),
+                imgW = path.attr("data-img-width"),
+                imgH = path.attr("data-img-height"),
+                ratio = imgW / imgH,
+                remainingH = 0,
+            // overflowing difference
+                diff = parseFloat(path.attr("data-diff"));
+            diff = diff ? diff : 0;
+            // remaining height to have fullscreen image only on parallax
+            
+            if(path.hasClass("parallax")){
+                var maxH = contH > windowH ? contH : windowH;
+                remainingH = windowH - contH;
+            }
+            // set img values depending on cont
+            imgH = contH + remainingH + diff;
+            imgW = imgH * ratio;
+            // fix when too large
+            if(contW > imgW){
+                imgW = contW;
+                imgH = imgW / ratio;
+            }
+            //
+            path.data("resized-imgW", imgW);
+            path.data("resized-imgH", imgH);
+            path.css("background-size", imgW + "px " + imgH + "px");
+        });
+    }
+    $(window).resize(backgroundResize);
+    $(window).focus(backgroundResize);
+    backgroundResize();
 });
