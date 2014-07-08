e5247d05e336fcd6bd945046748aa53bff110bbb
SergKazakov <sergeykazakoff@yandex.ru>
2014-07-08 19:41:51 +0400

    Use pixelperfect

--- packages/blog/app/js/main.js
+++ packages/blog/app/js/main.js
@@ -1,3 +1,16 @@
-$(function (){
-	'use strict';
+$(function () {
+    'use strict';
+    var dropdown = $('#dropdown');
+    $('.has-dropdown').click(function () {
+        if (dropdown.hasClass('dropdown-wr_active')) {
+            dropdown.removeClass('dropdown-wr_active');
+        }
+        else {
+            dropdown.addClass('dropdown-wr_active');
+        }
+    });
+    $('#close').click(function (e) {
+        e.preventDefault();
+        dropdown.addClass('dropdown-wr_active');
+    });
 });
