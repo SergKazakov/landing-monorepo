ee6bde6d2893038d609319dca4dd238ee35b7942
Sergey Kazakov <sergeykazakoff@yandex.ru>
2024-03-24 08:07:42 +0300

    :up: Update repositories

---
+++ packages/focal/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    'use strict';
+
+    //tabs
+    function getTabs(tabItem, tabContent) {
+        $('.' + tabContent + ':first-child').fadeIn();
+        $('.' + tabItem).on('click', function() {
+            $(this).addClass(tabItem + '--active')
+                .siblings()
+                .removeClass(tabItem + '--active');
+            $('.' + tabContent).eq($(this).index())
+                .fadeIn()
+                .siblings()
+                .hide();
+        });
+    }
+
+    getTabs('tab-controls__item', 'tab-item');
+
+
+    //nav
+    responsiveNav(".nav-collapse");
+    $('.nav-toggle').html('<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>');
+
+
+    //slider
+    var slider=$('#slides');
+
+    slider.superslides({
+        'animation': 'fade',
+        'play': 10000
+    });
+});
--- packages/oggetto-test-task/README.md
+++
@@ -1,2 +0,0 @@
-oggetto-test-task
-=================
